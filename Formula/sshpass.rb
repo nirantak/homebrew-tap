5fc6b0541c12e8f76dc3686178e34401e040cfc4
Nirantak Raghav <me@nirantak.com>
2021-03-10 17:52:33 +0530

    rufo formatting changes

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,17 +1,18 @@
-require 'formula'
+require "formula"
 
-class SSHpass < Formula
-  homepage 'https://sourceforge.net/projects/sshpass'
-  version '1.09'
+class Sshpass < Formula
+  homepage "https://sourceforge.net/projects/sshpass"
+  version "1.09"
   url "https://sourceforge.net/projects/sshpass/files/sshpass/#{version}/sshpass-#{version}.tar.gz"
-  sha256 '71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7'
+  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
 
-  depends_on 'gnu-sed'
+  depends_on "gnu-sed"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--prefix=#{prefix}"
     system "make install"
   end
 
